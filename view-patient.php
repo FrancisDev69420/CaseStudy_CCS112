cffc50bd98a831f825c56c0b33c6186400db27d4
jkRi0 <rioverosjustine92@gmail.com>
2025-02-26 10:54:31 +0800

    revert changes

--- view-patient.php
+++ view-patient.php
@@ -28,7 +28,7 @@
         }
 
         // Fetch SOAP data
-        $sqlSoap = "SELECT * FROM soap_notes ORDER BY last_visit DESC LIMIT 1";
+        $sqlSoap = "SELECT * FROM soap_notes WHERE patient_id = $patient_id";
         $resultSoap = $conn->query($sqlSoap);
 
         if ($resultSoap->num_rows > 0) {
